024691fe61c7e8a0c00055502268637a676a8960
Enrique <egl854@alumnos.unican.es>
2026-09-20 17:41:34 +0200

    Primer commit de prob2

---
+++ prob2/prob2.cpp
@@ -0,0 +1,31 @@
+/*
+ * prob2.cpp
+ *
+ *  Created on: 20 sept 2026
+ *      Author: Usuario
+ */
+#include <iostream>
+#include <cstdlib>
+
+int suma(int d1, int d2) {
+    return d1+d2;
+}
+
+
+int main(int narg, char *arg[]) {
+    int d1=50;
+    int d2=100;
+
+    if(narg > 1) {  // 1 argumento
+        d1=std::atoi(arg[1]);
+    }
+
+    if(narg > 2) {  // 2 argumentos
+        d2=std::atoi(arg[2]);
+    }
+    std::cout <<" suma " << d1 << " y " << d2 << " = " << suma(d1,d2) << std::endl;
+    return 0;
+}
+
+
+
